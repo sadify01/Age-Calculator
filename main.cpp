97380248c0f00fc1e1cf9d2404e3836cd2ec56e0
sadify <119104967+sadify01@users.noreply.github.com>
2023-02-16 18:49:32 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    int age;
+
+    std::cout << "How old are you?: ";
+    std::cin >> age;
+    std::cout << "You are " << age << " years old!";
+
+    return 0;
+}
